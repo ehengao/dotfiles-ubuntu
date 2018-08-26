999957c76d84f697660b177f7f2887b78570d832
Heng GAO <heng.gao.us@gmail.com>
2018-08-25 22:45:48 -0500

    zsh and its themes

---
+++ entry.sh
@@ -0,0 +1,3 @@
+export DOTFILES=$(dirname "$0")
+source $DOTFILES/path.sh
+source $DOTFILES/alias.sh
---
+++ path.sh
@@ -0,0 +1,8 @@
+export PATH=$PATH:/home/ehengao/.local/lib/go/go_1_11/bin
+export GOPATH=/home/ehengao/dev/go
+export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOBIN
+
+# added by Miniconda3 installer
+export PATH="/home/ehengao/.local/lib/miniconda3/bin:$PATH"
+. /home/ehengao/.local/lib/miniconda3/etc/profile.d/conda.sh
